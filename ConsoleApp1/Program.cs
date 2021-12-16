4cf459da7a2910159a031293e5d9e71d5d88b842
Mirza Atif Baig <65837135+Mirza62@users.noreply.github.com>
2021-12-16 23:52:24 +0530

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class PlusMinus
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Friends!Write the length of integer array");
+            int noflN = Convert.ToInt32(Console.ReadLine().Trim());
+            Console.WriteLine("Could you please provide the array value using spaces");
+            List<int> costarra = Console.ReadLine().TrimEnd().Split(' ').ToList()
+                .Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+            PlusMinus.plusMinus(costarra);
+        }
+
+
+        public static void plusMinus(List<int> arrayofint)
+        {
+            int totalnumberofelements = arrayofint.Count();
+            int positivenos =0 ;
+            int negativenos = 0;
+            int neutralnumber = 0;
+
+            foreach (var integer in arrayofint)
+            {
+                if (integer < 0)
+                {
+                    negativenos++;
+                }
+                else if (integer > 0)
+                {
+                    positivenos++;
+                }
+                else if (integer == 0)
+                {
+                    neutralnumber++;
+                }
+            }
+
+            decimal firstOutput = (decimal)positivenos / (decimal)totalnumberofelements;
+            decimal secondOutput = (decimal)negativenos / (decimal)totalnumberofelements;
+            decimal thirdOutput = (decimal)neutralnumber / (decimal)totalnumberofelements;
+            firstOutput = Math.Round(firstOutput, 6);
+            secondOutput = Math.Round(secondOutput, 6);
+            thirdOutput = Math.Round(thirdOutput, 6);
+            Console.WriteLine(firstOutput);
+            Console.WriteLine(secondOutput);
+            Console.WriteLine(thirdOutput);
+        }
+
+
+    }
+}
---
+++ MiniMaxSum/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniMaxSum
+{
+    class MiniMaxSum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Friends,Please enter the value of integer array, Thank you");
+            List<int> costarr = Console.ReadLine().Trim().Split(' ').ToList().Select(a => Convert.ToInt32(a)).ToList();
+            MiniMaxSum.MinimumMaximum(costarr);
+        }
+
+        public static void MinimumMaximum(List<int> arr)
+        {
+                List<long> arrLong = arr.Select(i => (long)i).ToList();
+                Console.WriteLine(((arrLong.Sum()) - arrLong.Max()) + " " + ((arrLong.Sum()) - arrLong.Min()));
+        }
+    }
+}
+/* My Solution
+//1
+foreach (var insidenumber in arr)
+{//5
+    if (number != insidenumber)
+    {
+        int a = number + insidenumber;
+        calculate.Add(a);
+    }
+}
+int value = calculate.Sum();
+calculate.Clear();
+Result.Add(value);
+}
+
+int minimumValue = Result.Min();
+int maximumValue = Result.Max();
+int mininumvalueinarray = arr.Min();
+int maximumvalueinarray = arr.Max();
+int result1 = minimumValue - maximumvalueinarray;
+int result2 = maximumValue - mininumvalueinarray;
+Console.WriteLine("the minimum value" + result1 + " " + "the maximum value" + result2);
+*/
+
+/*Next Best 
+ 
+             //var max = Int64.MinValue;
+            //var min = Int64.MaxValue;
+            //int sum = 0;
+            //foreach (var number in arr)
+            //{
+            //    sum += number;
+            //    max = Math.Max(number, max);
+            //    min = Math.Min(number, min);
+            //}
+            //Console.WriteLine((sum - max)+ " " + (sum-min));
+ 
+ 
+ */
+
+
+
