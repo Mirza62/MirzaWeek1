ca80578ac858683399ade5f2563da0bf2f9edacd
Mirza Atif Baig <65837135+Mirza62@users.noreply.github.com>
2021-12-17 22:41:00 +0530

    Time Conversion Problem
    need to figure out GetEnvironmentVariable

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(Environment.GetEnvironmentVariable(@".\Documents\mydoc.txt"), true);
+
+            string s = Console.ReadLine();
+
+            string result = TimeConversion.timeConversion(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+    public class TimeConversion
+    {
+        public static string timeConversion(string s)
+        {
+            DateTime myDate = DateTime.Parse(s);
+            return myDate.ToString("T", CultureInfo.CreateSpecificCulture("de-DE"));
+        }
+    }
+}
